ce374e1e79ea760aa9dcce34c7cdead5d5c05d5a
Guido Pellegrini <gpellegrini@movizen.com>
2024-01-06 19:03:23 -0300

    Full Project

---
+++ README.md
@@ -0,0 +1,25 @@
+# Responsive Landing Page - Galaxy Fold 4
+
+## Description
+
+Welcome to the Responsive Landing Page for the Galaxy Fold 4, a fictional and innovative product that pushes the boundaries of mobile technology. This landing page was crafted with a combination of HTML, CSS, and JavaScript, ensuring a seamless and interactive user experience.
+
+## Technologies Used
+
+- **HTML:** Provides the structure and layout of the landing page.
+- **CSS:** Styles the page, making it visually appealing and consistent.
+- **JavaScript:** Adds interactivity and dynamic behavior to the page.
+
+## Usage
+
+1. Clone the repository to your local machine.
+
+   ```bash
+   git clone https://github.com/your-username/galaxy-fold-4-landing-page.git
+   ```
+
+2. Open the `index.html` file in your preferred web browser to explore the landing page.
+
+## Preview
+
+![Galaxy Fold 4 Landing Page](./preview.png)
---
+++ assets/js/main.js
@@ -0,0 +1,125 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+/*===== MENU SHOW =====*/
+/* validamos si la constante existe */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/*===== MENU HIDDEN =====*/
+/* validamos si la constante existe */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // cuando hacemos click en nav__link, borramos la clase show-menu
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+  const header = document.getElementById("header");
+  // Cuando el scroll sea mayor que 50 viewport height, agregamos la clase scroll-header al tag header
+  if (this.scrollY >= 50) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
+
+/*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // Cuando el scroll sea mayor que 200 viewport height, agregamos la clase show-scroll al tag a con la clase scroll-top
+  if (this.scrollY >= 200) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
+
+/*=============== THEME ===============*/
+function changeTheme() {
+  const themeButton = document.getElementById("theme-button");
+  const themeIcon = document.getElementById("theme-button__icon");
+  const lightTheme = "light-theme";
+  const iconTheme = "ri-sun-fill";
+
+  // Obtenemos el tema actual del storage
+  const selectedTheme = localStorage.getItem("selected-theme");
+  const selectedIcon = localStorage.getItem("selected-icon");
+
+  // Obtenemos el tema actual que tiene la interfaz validando la clase dark-theme
+  const getCurrentTheme = () => (document.body.classList.contains(lightTheme) ? "light-theme" : "dark-theme");
+  const getCurrentIcon = () => (themeIcon.classList.contains(iconTheme) ? "ri-sun-fill" : "ri-moon-fill");
+
+  // Validamos si el usuario eligió un tema previamente
+  if (selectedTheme) {
+    // Si se cumple la validación, preguntamos cuál fue el problema para saber si activamos o desactivamos el dark theme
+    document.body.classList[selectedTheme === "light-theme" ? "add" : "remove"](lightTheme);
+    themeIcon.classList[selectedIcon === "ri-sun-fill" ? "add" : "remove"](iconTheme);
+  }
+
+  // Activamos / desactivamos el tema manualmente con el botón
+  themeButton.addEventListener("click", () => {
+    // Agregamos o quitamos el dark / icon theme
+    document.body.classList.toggle(lightTheme);
+    themeIcon.classList.toggle(iconTheme);
+
+    // Guardamos el tema y el icono actual que eligió el usuario
+    localStorage.setItem("selected-theme", getCurrentTheme());
+    localStorage.setItem("selected-icon", getCurrentIcon());
+  });
+}
+
+changeTheme();
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.add("active-link");
+    } else {
+      document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  distance: "60px",
+  duration: 1000,
+  delay: 0,
+  reset: false,
+});
+
+sr.reveal(`.home__header, .section__title`, { delay: 600 });
+sr.reveal(`.home__footer`, { delay: 700 });
+sr.reveal(`.home__img`, { delay: 900, origin: "top" });
+
+sr.reveal(`.sponsor__img, .products__card, .footer__logo, .footer__content, .footer__copy`, {
+  origin: "top",
+  interval: 100,
+});
+sr.reveal(`.specs__data, .gaming__animate`, { origin: "left", interval: 100 });
+sr.reveal(`.specs__img, .gaming__img`, { origin: "right" });
+sr.reveal(`.case__img`, { origin: "top" });
+sr.reveal(`.case__data`);
